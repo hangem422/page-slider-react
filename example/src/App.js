4df3c334baef47935691ac9795f41dd26092ef35
hangem422 <hangem422@gmail.com>
2020-03-01 12:17:17 +0900

    Modify #1 -  update the component path of the sample.

--- example/src/App.js
+++ example/src/App.js
@@ -1,11 +1,11 @@
 import React, { useMemo } from 'react';
 import { PageSlider } from '@hant/react-page-slider';
 
-import TestComp01 from './examples/TestComp01';
-import TestComp02 from './examples/TestComp02';
-import TestComp03 from './examples/TestComp03';
-import TestComp04 from './examples/TestComp04';
-import TestComp05 from './examples/TestComp05';
+import TestComp01 from './components/TestComp01';
+import TestComp02 from './components/TestComp02';
+import TestComp03 from './components/TestComp03';
+import TestComp04 from './components/TestComp04';
+import TestComp05 from './components/TestComp05';
 
 function App() {
   const compList = useMemo(
@@ -18,7 +18,7 @@ function App() {
         await new Promise(resolve => setTimeout(resolve, 2000));
         return TestComp05;
       },
-      () => import('./examples/TestComp06'),
+      () => import('./components/TestComp06'),
     ],
     [],
   );
